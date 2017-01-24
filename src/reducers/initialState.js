8671074e534b020e88a3619028f7cdbf39223da1
Derek Kniffin <derek.kniffin@gmail.com>
2017-01-24 17:03:04 -0500

    Shuffle the deck

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,5 +1,8 @@
 import Immutable from 'immutable'
-import deck from '../../decks/zombie.json'
+import rawdeck from '../../decks/zombie.json'
+import shuffle from 'array-shuffle'
+
+const deck = shuffle(rawdeck)
 
 export default Immutable.fromJS({
   deck: deck, // Contains all cards currently in the deck
