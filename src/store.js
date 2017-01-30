f7bd4c16053f8257aded47a17a7189160c042045
Derek Kniffin <derek.kniffin@gmail.com>
2017-01-30 08:43:18 -0500

    Add redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
 
 import reducer from "./reducers";
 
 import initialState from './reducers/initialState';
 
-const store = createStore(reducer, initialState, applyMiddleware(thunk));
+const store = createStore(reducer, initialState, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
 
 export default store;
