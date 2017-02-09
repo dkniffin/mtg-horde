0b845cbc3767ed9763108fc9e4f944f706cf375f
Derek Kniffin <derek.kniffin@gmail.com>
2017-02-09 09:28:26 -0500

    Convert CardZone to stateless component function

--- src/components/CardZone/index.js
+++ src/components/CardZone/index.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.scss';
 
 import Card from "../Card"
 
-class CardZone extends Component {
-  render() {
-    return (
-      <div id={this.props.id} className="CardZone">
-        {
-          this.props.cards.map((card) => {
-            const i = card.get('index')
-            const power = parseInt(card.getIn(['cardData', 'power']), 10) +
-                          card.getIn(['counters', 'power']) +
-                          card.getIn(['modifiers', 'power'])
-            const toughness = parseInt(card.getIn(['cardData', 'toughness']), 10) +
-                              card.getIn(['counters', 'toughness']) +
-                              card.getIn(['modifiers', 'toughness'])
+const CardZone = function({id, cards, onTap, onCardClick, location}) {
+  return (
+    <div id={id} className="CardZone">
+      {
+        cards.map((card) => {
+          const i = card.get('index')
+          const power = parseInt(card.getIn(['cardData', 'power']), 10) +
+                        card.getIn(['counters', 'power']) +
+                        card.getIn(['modifiers', 'power'])
+          const toughness = parseInt(card.getIn(['cardData', 'toughness']), 10) +
+                            card.getIn(['counters', 'toughness']) +
+                            card.getIn(['modifiers', 'toughness'])
 
-            const onTap = this.props.onTap ? () => this.props.onTap(i) : false
-            return <Card key={"card-" + i}
-                         imageUrl={card.getIn(['cardData', 'imageUrl'])}
-                         tapped={card.get('tapped')}
-                         onTap={onTap}
-                         power={power}
-                         toughness={toughness}
-                         onClick={() => this.props.onCardClick(this.props.location, card) }/>
-          })
-        }
-      </div>
-    );
-  }
+          return <Card key={"card-" + i}
+                       imageUrl={card.getIn(['cardData', 'imageUrl'])}
+                       tapped={card.get('tapped')}
+                       onTap={onTap ? () => onTap(i) : false}
+                       power={power}
+                       toughness={toughness}
+                       onClick={() => onCardClick(location, card) }/>
+        })
+      }
+    </div>
+  );
 }
 
 export default CardZone;
